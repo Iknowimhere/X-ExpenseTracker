077ca7a311f18fcba348215db82aff53b0ad9674
umashankar <umashankarp33@gmail.com>
2024-12-06 23:21:08 +0530

    add bar chart for top expenses

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,49 +1,53 @@
 import React from 'react';
 import './Transaction.css';
 import pizza from '../../assets/pizza.svg';
+import { TopExpenses } from './TopExpenses';
 
 const TransactionItem = ({ name, date, amount }) => (
-    <div className="list-item">
-        <div className="section1">
-            <img src={pizza} alt={name} />
-            <div className="section-content">
-                <p>{name}</p>
-                <p>{date}</p>
-            </div>
-        </div>
-        <div className="section2">
-            <p>{amount}</p>
-            <button className="action-btn">Delete</button>
-            <button className="action-btn">Edit</button>
-        </div>
+  <div className='list-item'>
+    <div className='section1'>
+      <img src={pizza} alt={name} />
+      <div className='section-content'>
+        <p>{name}</p>
+        <p>{date}</p>
+      </div>
     </div>
+    <div className='section2'>
+      <p>{amount}</p>
+      <button className='action-btn'>Delete</button>
+      <button className='action-btn'>Edit</button>
+    </div>
+  </div>
 );
 
 export const Main = () => {
-    const transactions = [
-        { name: 'Samosa', date: 'March 20, 2024', amount: 'R200' },
-        { name: 'Pizza', date: 'March 21, 2024', amount: 'R300' },
-        { name: 'Burger', date: 'March 22, 2024', amount: 'R150' },
-    ];
+  const transactions = [
+    { name: 'Samosa', date: 'March 20, 2024', amount: 'R200' },
+    { name: 'Pizza', date: 'March 21, 2024', amount: 'R300' },
+    { name: 'Burger', date: 'March 22, 2024', amount: 'R150' },
+  ];
 
-    return (
-        <div className='transaction-container'>
-            <div className="recent-transactions-container">
-            <h1 className="transaction-title">Recent Transactions</h1>
-            <div className='recent-transactions'>
-                <div className="list">
-                    {transactions.map((transaction, index) => (
-                        <React.Fragment key={index}>
-                            <TransactionItem {...transaction} />
-                            {index < transactions.length - 1 && <hr />}
-                        </React.Fragment>
-                    ))}
-                </div>
-            </div>
-            </div>
-            <div className="top-expenses">
-                <p>Bar Chart</p>
-            </div>
+  return (
+    <div className='transaction-container'>
+      <div className='recent-transactions-container'>
+        <h1 className='transaction-title'>Recent Transactions</h1>
+        <div className='recent-transactions'>
+          <div className='list'>
+            {transactions.map((transaction, index) => (
+              <React.Fragment key={index}>
+                <TransactionItem {...transaction} />
+                {index < transactions.length - 1 && <hr />}
+              </React.Fragment>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+      <div className='top-expenses-container'>
+        <h1>Top expenses</h1>
+        <div className='top-expenses'>
+          <TopExpenses />
+        </div>
+      </div>
+    </div>
+  );
 };
